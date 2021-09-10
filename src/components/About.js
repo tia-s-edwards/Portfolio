1860eeda735066ff9a285b7daab2f9a11f137472
Tia Edwards <tia@tias-mbp.myfiosgateway.com>
2021-09-10 02:39:08 -0400

    removed svg

--- src/components/About.js
+++ src/components/About.js
@@ -29,7 +29,7 @@ export default function About () {
           <img
             className="object-cover object-center rounded"
             alt="hero"
-            src="./femaleCoder.svg"
+            src="./coding.svg"
           />
         </div>
       </div>
