837a9ee3b4efbb5f2fdb41f1abb8d299e9271f12
Tia Edwards <tia@tias-mbp.myfiosgateway.com>
2021-09-10 02:08:56 -0400

    initial commit

---
+++ src/data.js
@@ -0,0 +1,19 @@
+export const projects = [
+  {
+    title: "Todo List",
+    subtitle: "React & Bootstrap",
+    description:
+      "Simple react CRUD (Create, Read, Update, Delete) application ",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML",
+  "CSS",
+  "TypeScript",
+  "Angular"
+];
